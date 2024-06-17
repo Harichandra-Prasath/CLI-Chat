b97d185886638758a8722f924bbc4778abb97ac9
Harichandra-Prasath <1hcp302004@gmail.com>
2024-06-17 10:25:45 +0530

    Announcement working

--- clients.go
+++ clients.go
@@ -1,13 +1,39 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+	"strings"
+)
 
-type Client struct {
-	Conn *net.TCPConn
+type ClientDetails struct {
+	Name string
 }
 
-func getClient(conn *net.TCPConn) *Client {
+type Client struct {
+	Details *ClientDetails
+	Conn    *net.TCPConn
+}
+
+func getClient(conn *net.TCPConn, details *ClientDetails) *Client {
 	return &Client{
-		Conn: conn,
+		Conn:    conn,
+		Details: details,
+	}
+}
+
+func (s *Server) handleMessage(client *Client, data string) {
+
+	data = strings.Replace(data, "\n", "", -1)
+	parts := strings.Split(data, " ")
+	mode := parts[0]
+	switch mode {
+	case "/announce":
+		s.announce(parts[1], client)
+	case "/whisper":
+		fmt.Println("Someone whispered")
+	default:
+		fmt.Println("didnt understand")
+		return
 	}
 }
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
 	"sync"
@@ -39,16 +40,47 @@ func (s *Server) Serve() {
 }
 
 func (s *Server) handleConn(conn *net.TCPConn) {
-	client := getClient(conn)
+	var det ClientDetails
+
+	conn.Write([]byte("Please give Client details in one line Json\n"))
+
+	det_buff := make([]byte, 56)
+	n, err := conn.Read(det_buff)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	det_buff = det_buff[:n-1]
+	err = json.Unmarshal(det_buff, &det)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	client := getClient(conn, &det)
+
 	s.lock.Lock()
 	s.Clients = append(s.Clients, client)
 	s.lock.Unlock()
+
 	for {
 		buff := make([]byte, 512)
 		_, err := client.Conn.Read(buff)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
-		fmt.Print(string(buff))
+		data := string(buff)
+		s.handleMessage(client, data)
 	}
+}
+
+func (s *Server) announce(message string, announcer *Client) {
+	s.lock.RLock()
+	for _, client := range s.Clients {
+		if client.Details.Name != announcer.Details.Name {
+			go func(c *Client) {
+				c.Conn.Write([]byte(fmt.Sprintf("%s Announced: %s\n", announcer.Details.Name, message)))
+			}(client)
+		}
+
+	}
+	s.lock.RUnlock()
 }
